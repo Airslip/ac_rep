c91ea8bf8e1645c56e4e6dbd86a97387099bc68d
Airslip <Grek2047@yandex.ru>
2016-09-29 17:41:43 +0400

    убрал надпись в футере

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -72,8 +72,6 @@ AppAsset::register($this);
 <footer class="footer">
     <div class="container">
         <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
-
-        <p class="pull-right"><?= Yii::powered() ?></p>
     </div>
 </footer>
 
--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -68,8 +68,6 @@ AppAsset::register($this);
 <footer class="footer">
     <div class="container">
         <p class="pull-left">&copy; Автодилер <?= date('Y') ?></p>
-
-        <p class="pull-right"><?= Yii::powered() ?></p>
     </div>
 </footer>
 
